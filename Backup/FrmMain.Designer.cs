de869c163da1e6bb272ae1d42cb816afca0a356c
mehmetkalayci <mmtkalayci@gmail.com>
2019-11-17 17:31:21 +0300

    backup projesi eklendi

---
+++ Backup/FrmMain.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Backup
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnBackup = new System.Windows.Forms.Button();
+            this.btnRestore = new System.Windows.Forms.Button();
+            this.folderBrowserBackup = new System.Windows.Forms.FolderBrowserDialog();
+            this.folderBrowserRestore = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(524, 281);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // btnBackup
+            // 
+            this.btnBackup.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBackup.Location = new System.Drawing.Point(437, 246);
+            this.btnBackup.Name = "btnBackup";
+            this.btnBackup.Size = new System.Drawing.Size(75, 23);
+            this.btnBackup.TabIndex = 0;
+            this.btnBackup.Text = "Yedekle";
+            this.btnBackup.UseVisualStyleBackColor = true;
+            this.btnBackup.Click += new System.EventHandler(this.btnBackup_Click);
+            // 
+            // btnRestore
+            // 
+            this.btnRestore.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRestore.Location = new System.Drawing.Point(356, 246);
+            this.btnRestore.Name = "btnRestore";
+            this.btnRestore.Size = new System.Drawing.Size(75, 23);
+            this.btnRestore.TabIndex = 1;
+            this.btnRestore.Text = "Geri Yükle";
+            this.btnRestore.UseVisualStyleBackColor = true;
+            this.btnRestore.Click += new System.EventHandler(this.btnRestore_Click);
+            // 
+            // folderBrowserBackup
+            // 
+            this.folderBrowserBackup.Description = "Verilerinizi yedeklemek istediğiniz klasörü seçin.";
+            // 
+            // folderBrowserRestore
+            // 
+            this.folderBrowserRestore.Description = "Yedeğinizin bulunduğu klasörü seçin.";
+            this.folderBrowserRestore.ShowNewFolderButton = false;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 281);
+            this.Controls.Add(this.btnRestore);
+            this.Controls.Add(this.btnBackup);
+            this.Controls.Add(this.richTextBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kira Takip | Yedekle / Geri yükle";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnBackup;
+        private System.Windows.Forms.Button btnRestore;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserBackup;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserRestore;
+    }
+}
+
---
+++ Backup/FrmMain.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Backup
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBackup_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (folderBrowserBackup.ShowDialog() == DialogResult.OK)
+                {
+                    // Pick the data source dir
+                    string sourceDataPath = Application.StartupPath + "\\Data";
+
+                    // Make target dir
+                    string targetBackupPath = folderBrowserBackup.SelectedPath + "\\" + DateTime.Now.ToString("yyyyMMddHHmmss");
+                    Directory.CreateDirectory(targetBackupPath);
+
+                    // Find db files
+                    string[] files = System.IO.Directory.GetFiles(sourceDataPath);
+
+                    // Copy src files to target
+                    foreach (string file in files)
+                    {
+                        string targetFilePath = System.IO.Path.Combine(targetBackupPath, Path.GetFileName(file));
+                        System.IO.File.Copy(file, targetFilePath, true);
+
+                        richTextBox1.Text += string.Format("{0} --> {1} kopyalandı...\n", file, targetFilePath);
+                    }
+                    richTextBox1.Text += "Yedekleme işlemi tamamlandı!\n";
+                    MessageBox.Show("Yedekleme işlemi tamamlandı!");
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+
+        }
+
+        private void btnRestore_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string targetDataPath = Application.StartupPath + "\\Data\\";
+
+
+                if (folderBrowserRestore.ShowDialog() == DialogResult.OK)
+                {
+                    string sourceDataPath = folderBrowserRestore.SelectedPath;
+
+                    string[] files = System.IO.Directory.GetFiles(sourceDataPath);
+
+                    foreach (var file in files)
+                    {
+                        string ext = Path.GetExtension(file);
+
+                        if (ext == ".mdf" || ext == ".ldf")
+                        {
+                            string target = targetDataPath + Path.GetFileName(file);
+                            File.Copy(file, target);
+                            richTextBox1.Text += string.Format("{0} --> {1} kopyalandı...\n", file, target);
+                        }
+                    }
+                    richTextBox1.Text += "Geri yükleme işlemi tamamlandı!\n";
+                    MessageBox.Show("Geri yükleme işlemi tamamlandı!");
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+        }
+    }
+}
